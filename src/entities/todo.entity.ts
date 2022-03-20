d62a2a15a097007edea96e614c50f6e461e0fe28
Alviannn <alvianndq@gmail.com>
2022-03-21 01:08:35 +0700

    Add luxon date to Todo entity

--- src/entities/todo.entity.ts
+++ src/entities/todo.entity.ts
@@ -1,7 +1,21 @@
+import { DateTime } from 'luxon';
 import {
     BaseEntity, Entity,
-    Column, PrimaryGeneratedColumn
+    Column, PrimaryGeneratedColumn,
+    ValueTransformer
 } from 'typeorm';
+
+/**
+ * Since the database can only accept {@link Date}
+ * we have to make it transform from {@link DateTime} to {@link Date}
+ *
+ * Well, I want to use {@link DateTime} from luxon
+ * so that I can easily format it using {@link Todo.DATE_FORMAT}
+ */
+const dateTransformer: ValueTransformer = {
+    from: (date: Date) => DateTime.fromJSDate(date),
+    to: (date: DateTime) => date.toJSDate()
+};
 
 @Entity('todos')
 export class Todo extends BaseEntity {
@@ -12,10 +26,20 @@ export class Todo extends BaseEntity {
     @Column({ name: 'full_name', length: 64 })
     content!: 128;
 
-    @Column({ name: 'created_at', type: 'date', default: new Date() })
-    createdAt!: Date;
+    @Column({
+        name: 'created_at',
+        type: 'timestamp',
+        transformer: dateTransformer,
+        default: DateTime.utc()
+    })
+    createdAt!: DateTime;
 
-    @Column({ name: 'updated_at', type: 'date', default: new Date() })
+    @Column({
+        name: 'updated_at',
+        type: 'timestamp',
+        transformer: dateTransformer,
+        default: DateTime.utc()
+    })
     updatedAt!: Date;
 
 }
