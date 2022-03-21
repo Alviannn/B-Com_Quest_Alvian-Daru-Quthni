500add4a0d6b03207e0d6336086ca3462cde2353
Alviannn <alvianndq@gmail.com>
2022-03-21 16:42:51 +0700

    Change default port to 5000

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { connectionConfig } from './ormconfig';
 import { createConnection } from 'typeorm';
 import { app } from './app';
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 5000;
 
 app.listen(port, async () => {
     await createConnection(connectionConfig);
