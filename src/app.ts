da209d88599920bd2a6a8ba2968587fe7e251ee5
Alviannn <alvianndq@gmail.com>
2022-03-21 19:20:55 +0700

    Fix empty body

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,13 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-// routes
-app.use('/v1/auth', authRouter);
-app.use('/v1/articles', articleRouter, commentRouter);
-
 app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/v1/auth', authRouter);
+app.use('/v1/articles', articleRouter, commentRouter);
 
 export { app };
